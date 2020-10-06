c4007fad54d7e6098bbdc18cee907775e537c581
EduardoHi <eshidalgo97@gmail.com>
2020-10-05 20:02:47 -0500

    Component Example to use Google Auth
    Nuevo componente `GoogleBtn` como ejemplo de como utilizar los
    componentes `GoogleLogin` y `GoogleLogout` de la libreria
    react-google-login.
    
    Lo iba a conectar con el resto del Auth, pero no entiendo muy bien como
    está todo conectado. lo más importante es el callback `onSuccess` que
    recibe `GoogleLogin` ahí es donde se recibe la información de parte de
    google para ser usada por la webapp, así como el tokenId y el
    accessToken para ser pasados al backend.

--- client/src/components/auth/login/Login.tsx
+++ client/src/components/auth/login/Login.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import useAuth from '../../../utils/hooks/useAuth';
+import GoogleBtn from '../../../GoogleBtn';
 
 const Login: React.FC = () => {
   const { authenticated, loading } = useAuth();
   if (loading) return <div>Cargando...</div>;
   if (authenticated) return <div>Autenticado! Te debiste haber redirigido a /</div>;
-  return <div>No Autenticado! Debes estar viendo este mensaje ya que debes iniciar sesión</div>;
+  return <GoogleBtn />;
 };
 
 export default Login;
